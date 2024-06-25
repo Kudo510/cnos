b50243634fee74b7f51d6e1b84a001159705fb6a
Cuong Van Dam <cuong.van-dam@moresophy.com>
2024-06-25 11:02:02 +0200

    create 162 synthetic templates

--- cnos_analysis/real_images_templates/icbin/analysis_inference_custom.sh
+++ cnos_analysis/real_images_templates/icbin/analysis_inference_custom.sh
@@ -1,4 +1,4 @@
-export OUTPUT_DIR=datasets/bop23_challenge/datasets/templates_pyrender/icbin_162/obj_000001
+export OUTPUT_DIR=cnos_analysis/real_images_templates/icbin_162/obj_000001
 export RGB_PATH=cnos_analysis/crop_proposals/000001.png
 
 python -m cnos_analysis.real_images_templates.icbin.analysis_inference_custom --template_dir $OUTPUT_DIR --rgb_path $RGB_PATH --stability_score_thresh 0.97
