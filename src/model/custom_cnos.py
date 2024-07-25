1be0f1966d6697a43e07559ef4aa8f516f66d656
Cuong Van Dam <cuong.van-dam@moresophy.com>
2024-07-25 13:37:38 +0200

    update

--- src/model/custom_cnos.py
+++ src/model/custom_cnos.py
@@ -1,36 +1,43 @@
-from src.model.foundpose import resize_and_pad_image
-
-import torchvision.transforms as T
-from PIL import Image
-import torch
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
-from src.model.loss import PairwiseSimilarity, Similarity
-from torchvision.io import read_image
-import math
-import os
-import sys
-import shutil
-from tqdm import tqdm
-import time
-import logging
-import os.path as osp
-from hydra import initialize, compose
-from hydra.utils import instantiate
 import argparse
 import glob
-from src.utils.bbox_utils import CropResizePad
-from omegaconf import DictConfig, OmegaConf
-from torchvision.utils import save_image
-from src.model.utils import Detections, convert_npz_to_json
-from src.utils.inout import save_json_bop23
+import logging
+import math
+import multiprocessing
+import os
+import shutil
+import sys
+import time
+from functools import partial
+from types import SimpleNamespace
+
 import cv2
 import distinctipy
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torchvision.transforms as T
+import torchvision.utils
+from hydra import compose, initialize
+from hydra.utils import instantiate
+from omegaconf import DictConfig, OmegaConf
+from PIL import Image
+from pycocotools import mask as mutils
+from pycocotools import _mask as coco_mask
 from skimage.feature import canny
 from skimage.morphology import binary_dilation
+from sklearn.decomposition import PCA
+from tqdm import tqdm
+from torchvision.io import read_image
+from torchvision.ops import masks_to_boxes
+
 from segment_anything.utils.amg import rle_to_mask
-from types import SimpleNamespace
+from src.model.foundpose import resize_and_pad_image
+from src.model.loss import PairwiseSimilarity, Similarity
+from src.model.utils import BatchedData, Detections, convert_npz_to_json
+from src.utils.bbox_utils import CropResizePad
+from src.utils.inout import save_json_bop23, load_json
+
 
 # set level logging
 logging.basicConfig(level=logging.INFO)
@@ -210,11 +217,11 @@ def modified_cnos_crop_feature_extraction(crop_rgb, dino_model, device):
             scaled_padded_crop_rgb.to(device), n=layers_list, return_class_token=True)[18][0].reshape(-1,1024).to("cpu")
     del dino_model
 
-    # PCA
-    pca = PCA(n_components=3, random_state=5)
-    pca_patches_descriptors = pca.fit_transform(np.array(feature_patches)).flatten()
+    # # PCA
+    # pca = PCA(n_components=3, random_state=5)
+    # pca_patches_descriptors = pca.fit_transform(np.array(feature_patches)).flatten()
 
-    return torch.tensor(pca_patches_descriptors).unsqueeze(0) # patch_features.squeeze().to("cuda:0")
+    return feature_patches # torch.tensor(pca_patches_descriptors).unsqueeze(0) # patch_features.squeeze().to("cuda:0")
 
 
 def cnos_crop_feature_extraction(crop_rgb, dino_model, device):
@@ -331,14 +338,164 @@ def modified_cnos_templates_feature_extraction(templates, dino_model, num_templa
     del dino_model
 
     # PCA
-    pca = PCA(n_components=3, random_state=5)
-    pca_patches_descriptors = [pca.fit_transform(np.array(patch_feature)).flatten() for patch_feature in patch_features]
+    # pca = PCA(n_components=3, random_state=5)
+    # pca_patches_descriptors = [pca.fit_transform(np.array(patch_feature)).flatten() for patch_feature in patch_features]
 
-    return torch.tensor(pca_patches_descriptors)# patch_features.squeeze().to("cuda:0")
-
-
+    return patch_features, # torch.tensor(pca_patches_descriptors)# patch_features.squeeze().to("cuda:0")
 
 
+def custom_detections(sam_detections, proposals_features, real_ref_features, file_path, scene_id=1, frame_id=1):
+    # rgb_path = "cnos_analysis/crop_proposals/000001.png"
+    # template_dir = "foundpose_analysis/icbin/templates/test_images_templates/obj_000001_original"
+
+    # rgb = Image.open(rgb_path).convert("RGB")
+    detections = Detections(sam_detections)
+    mask_post_processing = SimpleNamespace(
+        min_box_size=0.05,  # relative to image size
+        min_mask_size=3e-4  # relative to image size
+    )
+
+    # get scores per proposal
+    cosine_metric = PairwiseSimilarity()
+    scores = cosine_metric(proposals_features, real_ref_features.unsqueeze(dim=0))
+    score_per_proposal_and_object = torch.topk(scores, k=5, dim=-1)[0]
+    score_per_proposal_and_object = torch.mean(
+        score_per_proposal_and_object, dim=-1
+    )
+    # assign each proposal to the object with highest scores
+    score_per_proposal, assigned_idx_object = torch.max(
+        score_per_proposal_and_object, dim=-1
+    )  # N_query
+
+    idx_selected_proposals = torch.arange(
+        len(score_per_proposal), device=score_per_proposal.device
+    )[score_per_proposal > 0.5]
+    pred_idx_objects = assigned_idx_object[idx_selected_proposals]
+    pred_scores = score_per_proposal[idx_selected_proposals]
 
 
+    # keep only detections with score > conf_threshold
+    detections.filter(idx_selected_proposals)
+    detections.add_attribute("scores", pred_scores)
+    detections.add_attribute("object_ids", pred_idx_objects)
+    detections.apply_nms_per_object_id(
+        nms_thresh=0.5
+    )
+    detections.to_numpy()
+
+    for ext in [".json", ".npz"]:
+        file = file_path + ext
+        if os.path.exists(file):
+            os.remove(file)
+            print(f"File {file} has been deleted.")
+        else:
+            print(f"File {file} does not exist.")
+
+    # save detections to file
+    results = detections.save_to_file(
+        scene_id=int(scene_id),
+        frame_id=int(frame_id),
+        runtime=1,
+        file_path=file_path,
+        dataset_name="icbin",
+        return_results=True,
+    )
+
+    num_workers = 10
+    result_paths = [file_path+".npz"]
+    pool = multiprocessing.Pool(processes=num_workers)
+    convert_npz_to_json_with_idx = partial(
+        convert_npz_to_json,
+        list_npz_paths=result_paths,
+    )
+    detections = list(
+        tqdm(
+            pool.imap_unordered(
+                convert_npz_to_json_with_idx, range(len(result_paths))
+            ),
+            total=len(result_paths),
+            desc="Converting npz to json",
+        )
+    )
+    formatted_detections = []
+    for detection in tqdm(detections, desc="Loading results ..."):
+        formatted_detections.extend(detection)
+
+    detections_path = f"{file_path}.json"
+    save_json_bop23(detections_path, formatted_detections)
+    print(f"Saved predictions to {detections_path}")
+
+
+def custom_visualize(input_file, dataset_name, rgb_path) -> None:
+    if dataset_name in ["hb", "tless"]:
+        split = "test_primesense"
+    else:
+        split = "test"
+        
+    num_max_objs = 50
+    green = [0, 1, 0]  # RGB values for green
+    colors = [green] * num_max_objs  # Create a list of green colors
+    
+    logging.info("Loading detections...")
+    with open(input_file, 'r') as f:
+        dets = json.load(f)
+    
+    conf_threshold = 0.5
+    dets = [det for det in dets if det['score'] > conf_threshold] # keeps the det if it has score > threshold
+    logging.info(f'Keeping only {len(dets)} detections having score > {conf_threshold}') # keeps the det if it has score > threshold
+    
+    
+    # sort by (scene_id, frame_id)
+    dets = sorted(dets, key=lambda x: (x['scene_id'], x['image_id']))
+    list_scene_id_and_frame_id = [(det['scene_id'], det['image_id']) for det in dets]
+    
+    output_dir = "output_cnos_analysis_5" 
+    os.makedirs(output_dir, exist_ok=True)
+    counter = 0
+    for scene_id, image_id in tqdm(list_scene_id_and_frame_id):
+        img = Image.open(rgb_path)
+        rgb = img.copy()
+        img = np.array(img)
+        masks, object_ids, scores = [], [], []
+        for det in dets:
+            if det['scene_id'] == scene_id and det['image_id'] == image_id:
+                masks.append(rle_to_mask(det['segmentation']))
+                object_ids.append(det['category_id']-1)  ## category_id is object_id
+                scores.append(det['score'])
+        # color_map = {obj_id: color for obj_id in np.unique(object_ids)}
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        img = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)
+        # img = (255*img).astype(np.uint8)
+        
+        alpha = 0.33
+        for mask_idx, mask in enumerate(masks):
+            edge = canny(mask)
+            edge = binary_dilation(edge, np.ones((2, 2)))
+            obj_id = object_ids[mask_idx]
+            temp_id = obj_id - 1
+
+            r = int(255*colors[temp_id][0])
+            g = int(255*colors[temp_id][1])
+            b = int(255*colors[temp_id][2])
+            img[mask, 0] = alpha*r + (1 - alpha)*img[mask, 0]
+            img[mask, 1] = alpha*g + (1 - alpha)*img[mask, 1]
+            img[mask, 2] = alpha*b + (1 - alpha)*img[mask, 2]   
+            img[edge, :] = 255
+
+        scene_dir = f"{output_dir}/{dataset_name}{scene_id:06d}" 
+        os.makedirs(scene_dir, exist_ok=True)
+        save_path = f"{scene_dir}/{image_id:06d}.png"
+        img = Image.fromarray(np.uint8(img))
+        img.save(save_path)
+        prediction = Image.open(save_path)
+        # concat side by side in PIL
+        img = np.array(img)
+        concat = Image.new('RGB', (img.shape[1] + prediction.size[0], img.shape[0]))
+        concat.paste(rgb, (0, 0))
+        concat.paste(prediction, (img.shape[1], 0))
+        # concat.save(save_path)
+        # if counter % 10 == 0:
+        #     logging.info(f"Saving {save_path}")
+        # counter+=1
+    return concat
 
